91cd94c15246b2261ba24170e5b4839abba5d04e
qwe3804132 <liupeiran9324@outlook.com>
2019-02-25 10:07:44 -0500

    add mlab support

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books308"
+  "URI": "mongodb://qwe3804132:z1324356@ds058739.mlab.com:58739/comp308-w2019-lesson4a"
   //Remote MongoDB deployment -> let 
   //"URI" :"mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
 };
